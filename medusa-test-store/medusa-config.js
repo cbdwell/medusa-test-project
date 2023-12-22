7cf3574c31866a8d1e5f3af7fa000ba15f294523
dspamer <dawidspamer@gmail.com>
2023-12-21 22:43:36 -0800

    Adding CORS 7

--- medusa-test-store/medusa-config.js
+++ medusa-test-store/medusa-config.js
@@ -22,19 +22,19 @@ try {
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-// const ADMIN_CORS =
-//   process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 // const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Allow all HTTP requests
 // ADMIN_CORS=/https:\/\/*/
-ADMIN_CORS="https://medusa-test-project-6xdkl8w5z-cbdwells-projects.vercel.app/"
+ADMIN_CORS="https://medusa-test-project.vercel.app"
 
 // CORS FOR PRODUCTION //
 // Allow any origin ending with vercel.app. For example, storefront.vercel.app
-STORE_CORS=/vercel\.app$/
+// STORE_CORS=/vercel\.app$/
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
